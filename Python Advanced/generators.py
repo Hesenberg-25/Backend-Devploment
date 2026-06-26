64536d843c55941b210cae97c8868abc3b34c355
Durvesh Patil <durveshspatil5@gmail.com>
2026-06-26 19:58:27 +0530

    Generator complte Theroy with Advantages Usage

--- Python Advanced/generators.py
+++ Python Advanced/generators.py
@@ -1,3 +1,5 @@
+# Here we are returning the amswer in form of result which is a list
+
 def square(nums):
     result=[]
     for i in nums:
@@ -9,4 +11,83 @@ square_nums=square([1,2,3,4])
 
 print(square_nums)
 
-# Here we are using return to give us result which is a list
+
+# Here we are using the keyword 'yield' to Access the Generators
+def square_num(nums):
+    for i in nums:
+        yield(i*i)
+
+squarenum=square_num([1,2,3,4,5])
+
+# To print the results we can use two methods
+
+# By using the next keymord sufficient number of times
+# If it exceeds compiler will throw StopIteration Error
+print(next(squarenum))
+print(next(squarenum))
+print(next(squarenum))
+print(next(squarenum))
+print(next(squarenum))
+
+#Using the for Loop which stops before the error of StopIteration
+for nums in squarenum:
+    print(nums)
+
+# Other way to COde to get list of Squares : 
+numsquare=[x*x for x in [1,2,3,4,5]]
+print(numsquare)
+
+# To Convert to Generator : 
+numsquare=(x*x for x in [1,2,3,4,5])
+print(numsquare)
+
+for i in numsquare:
+    print (i)
+
+# You can convert it to List by just using "list(numsquare)"
+ 
+# Advantage of Generators over List
+# 1. Readablity of the Code Increaes
+# 2. As Generators do not store the Data this gives us Edge over Perfomance
+
+# import memory_profiler
+# import random
+# import time
+
+# names = ['John', 'Corey', 'Adam', 'Steve', 'Rick', 'Thomas']
+# majors = ['Math', 'Engineering', 'CompSci', 'Arts', 'Business']
+
+# print 'Memory (Before): {}Mb'.format(mem_profile.memory_usage_psutil())
+
+# def people_list(num_people):
+#     result = []
+#     for i in xrange(num_people):
+#         person = {
+#                     'id': i,
+#                     'name': random.choice(names),
+#                     'major': random.choice(majors)
+#                 }
+#         result.append(person)
+#     return result
+
+# def people_generator(num_people):
+#     for i in xrange(num_people):
+#         person = {
+#                     'id': i,
+#                     'name': random.choice(names),
+#                     'major': random.choice(majors)
+#                 }
+#         yield person
+
+# # t1 = time.clock()
+# # people = people_list(1000000)
+# # t2 = time.clock()
+
+# t1 = time.clock()
+# people = people_generator(1000000)
+# t2 = time.clock()
+
+# print 'Memory (After) : {}Mb'.format(mem_profile.memory_usage_psutil())
+# print 'Took {} Seconds'.format(t2-t1)
+
+
---
+++ Python Advanced/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+numsquare=(x*x for x in [1,2,3,4,5])
+print(numsquare)
+
+for i in numsquare:
+    print (i)
