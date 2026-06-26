6402a6602f81a0a395f4f47d3b075e992ae1759e
Durvesh Patil <durveshspatil5@gmail.com>
2026-06-26 20:00:52 +0530

    Use of Advantage Shown

--- Python Advanced/generators.py
+++ Python Advanced/generators.py
@@ -50,44 +50,44 @@ for i in numsquare:
 # 1. Readablity of the Code Increaes
 # 2. As Generators do not store the Data this gives us Edge over Perfomance
 
-# import memory_profiler
-# import random
-# import time
+import memory_profiler
+import random
+import time
 
-# names = ['John', 'Corey', 'Adam', 'Steve', 'Rick', 'Thomas']
-# majors = ['Math', 'Engineering', 'CompSci', 'Arts', 'Business']
+names = ['John', 'Corey', 'Adam', 'Steve', 'Rick', 'Thomas']
+majors = ['Math', 'Engineering', 'CompSci', 'Arts', 'Business']
 
-# print 'Memory (Before): {}Mb'.format(mem_profile.memory_usage_psutil())
+print 'Memory (Before): {}Mb'.format(mem_profile.memory_usage_psutil())
 
-# def people_list(num_people):
-#     result = []
-#     for i in xrange(num_people):
-#         person = {
-#                     'id': i,
-#                     'name': random.choice(names),
-#                     'major': random.choice(majors)
-#                 }
-#         result.append(person)
-#     return result
+def people_list(num_people):
+    result = []
+    for i in xrange(num_people):
+        person = {
+                    'id': i,
+                    'name': random.choice(names),
+                    'major': random.choice(majors)
+                }
+        result.append(person)
+    return result
 
-# def people_generator(num_people):
-#     for i in xrange(num_people):
-#         person = {
-#                     'id': i,
-#                     'name': random.choice(names),
-#                     'major': random.choice(majors)
-#                 }
-#         yield person
-
-# # t1 = time.clock()
-# # people = people_list(1000000)
-# # t2 = time.clock()
+def people_generator(num_people):
+    for i in xrange(num_people):
+        person = {
+                    'id': i,
+                    'name': random.choice(names),
+                    'major': random.choice(majors)
+                }
+        yield person
 
 # t1 = time.clock()
-# people = people_generator(1000000)
+# people = people_list(1000000)
 # t2 = time.clock()
 
-# print 'Memory (After) : {}Mb'.format(mem_profile.memory_usage_psutil())
-# print 'Took {} Seconds'.format(t2-t1)
+t1 = time.clock()
+people = people_generator(1000000)
+t2 = time.clock()
+
+print 'Memory (After) : {}Mb'.format(mem_profile.memory_usage_psutil())
+print 'Took {} Seconds'.format(t2-t1)
 
 
